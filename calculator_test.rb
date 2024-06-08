a25fc68596926dde4759a6fae46166f8ecb45ef1
GK <ganesh.kaliannan@gmail.com>
2024-06-08 07:13:17 +0530

    calculate single string literal

--- calculator_test.rb
+++ calculator_test.rb
@@ -7,4 +7,16 @@ class CalculatorTest < Test::Unit::TestCase
     @calculator = Calculator.new
   end
 
+  def test_add_empty_string
+    result = @calculator.add("")
+    # Check the result if it is equal to 0
+    assert_equal 0, result
+  end
+
+  def test_add_single_digit_string
+    result = @calculator.add("1")
+    # Check result if it is equal to 0
+    assert_equal 1, result
+  end
+
 end
