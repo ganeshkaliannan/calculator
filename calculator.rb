08e00e92a95d1081d643ae436c57e9ad93e3f526
GK <ganesh.kaliannan@gmail.com>
2024-06-08 07:38:16 +0530

    handle negative numbers

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,12 @@ class Calculator
 
   def add(str)
     format_string = str.scan(/-?\d+/).map(&:to_i)
-    format_string.sum
+    negative_collection = format_string.count(&:negative?)
+     if negative_collection > 0
+        negative_numbers = format_string.reject(&:positive?)
+        p "Input data contains the following negative numbers ---- #{negative_numbers.join(",")} ----, please try again."
+     else
+        format_string.sum
+     end
   end
-
 end
