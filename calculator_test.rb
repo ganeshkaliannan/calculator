261ca71159a128eeff6cf1a5531a2eb8a0074999
GK <ganesh.kaliannan@gmail.com>
2024-06-08 06:23:06 +0530

    test to initialize the class

--- calculator_test.rb
+++ calculator_test.rb
@@ -1,5 +1,10 @@
 require "test/unit"
+require_relative './calculator'
 
 class CalculatorTest < Test::Unit::TestCase
+  def setup
+    # Create an instance of the Calculator class
+    @calculator = Calculator.new
+  end
 
 end
